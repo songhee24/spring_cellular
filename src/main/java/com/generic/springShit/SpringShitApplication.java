391ab1830981e5580d02be3dff58ce0d7cbe07df
Azamat Orozbaev <44497623+songhee24@users.noreply.github.com>
2020-04-09 20:36:41 +0600

    start

---
+++ src/main/java/com/generic/springShit/SpringShitApplication.java
@@ -0,0 +1,13 @@
+package com.generic.springShit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generic/springShit/SpringShitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generic.springShit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
