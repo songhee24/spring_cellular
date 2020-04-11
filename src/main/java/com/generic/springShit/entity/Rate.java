c666044a90f17f5db77c2a567fc47ef4865e941a
ERERkin <erkin1399@gmail.com>
2020-04-11 16:26:47 +0600

    Added get, post, delete and get all operation

--- src/main/java/com/generic/springShit/entity/Rate.java
+++ src/main/java/com/generic/springShit/entity/Rate.java
@@ -1,4 +1,33 @@
 package com.generic.springShit.entity;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Rate")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Rate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "for_call")
+    private Double forCall;
+
+    @Column(name = "for_sms")
+    private Double forSMS;
+
+    @ManyToOne
+    @JoinColumn(name = "operator_id", referencedColumnName = "id")
+    private Operator operatorId;
 }
